09800d27d474ceeee9c25de2490a66ad81e889fa
hyperlearn-ai <hyper.learn@gmail.com>
2025-03-20 20:49:06 +0000

    Added basic website structure

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>HyperLearn</h1>
+        <p>Your Free AI Learning Assistant</p>
+        <button>Try Free Now</button>
+      </div>
+    );
+  }
